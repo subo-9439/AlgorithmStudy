d276715a85120d2209faa9fe9729a0957db88b93
Sohee Lee <74662808+leesh26@users.noreply.github.com>
2022-05-25 01:45:09 +0900

    Update sohee.java

--- 3주차/소수 찾기/sohee.java
+++ 3주차/소수 찾기/sohee.java
@@ -5,7 +5,7 @@ import java.util.Set;
 class Solution {
     static Set<Integer> set = new HashSet<>();
     
-    void permulation(String input, String output, boolean[] visited, int limit){
+    void permutation(String input, String output, boolean[] visited, int limit){
         // 길이가 모두 채워지면 return
         if (output.length() == limit) {
             set.add(Integer.parseInt(output));
@@ -15,7 +15,7 @@ class Solution {
         for (int i = 0; i < input.length(); i ++){
             if (!visited[i]){
                 visited[i] = true; // 방문 처리
-                permulation(input, output + input.charAt(i), visited, limit);
+                permutation(input, output + input.charAt(i), visited, limit);
                 visited[i] = false; // 방문 해제
             }
         }
@@ -27,7 +27,7 @@ class Solution {
         // 주어진 numbers로 만들 수 있는 모든 경우의 수 구하기
         boolean[] visited = new boolean[numbers.length()];
         for (int i = 0; i < numbers.length(); i++){
-            permulation(numbers, "", visited, i + 1);
+            permutation(numbers, "", visited, i + 1);
         }
 
         // 소수 리스트 만들기
