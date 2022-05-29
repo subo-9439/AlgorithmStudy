86e04b55d2c2561997531a9cfce49e70544361ce
liean17 <94332594+liean17@users.noreply.github.com>
2022-05-29 16:23:31 +0900

    [3주차] <강도경> 방 번호, 재귀함수가 뭔가요?

--- 3주차/재귀함수가 뭔가요/dokyung.java
+++ 3주차/재귀함수가 뭔가요/dokyung.java
@@ -7,7 +7,7 @@ public class dokyung {
         int n = sc.nextInt();
         System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
         recursive(n);
-    }
+    } 
     public static void recursive(int n){
         String line = underLine;
 
