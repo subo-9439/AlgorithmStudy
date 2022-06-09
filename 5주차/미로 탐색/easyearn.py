1bda0eceb821240263217140e9afabf8327abae9
easyearn77 <lju8702@gmail.com>
2022-06-09 22:12:08 +0900

    [5주차]<이지언> 미로탐색

--- 5주차/미로 탐색/easyearn.py
+++ 5주차/미로 탐색/easyearn.py
@@ -4,9 +4,9 @@ from sys import stdin
 #입력받기
 n,m=map(int,input().split())
 graph=[stdin.readline().rstrip() for _ in range(n)]
-visited=[[False]*(m) for _ in range(n)]
+visited=[[0]*(m) for _ in range(n)]
 
-#move -> left, down, up, right
+#move -> right, down, up, left
 dx=[0,1,-1,0] 
 dy=[1,0,0,-1]
 
@@ -14,7 +14,7 @@ dy=[1,0,0,-1]
 def bfs(start,graph,visited):
     x,y=start[0]-1,start[1]-1
     queue=[(x,y)]
-    visited[x][y]=True
+    visited[x][y]=1
 
     while queue:
         x,y=queue.pop(0)
@@ -25,7 +25,7 @@ def bfs(start,graph,visited):
             nx=x+dx[i]
             ny=y+dy[i]
             if 0<=nx<n and 0<=ny<m:
-                if graph[nx][ny]=='1' and visited[nx][ny]==False:
+                if graph[nx][ny]=='1' and visited[nx][ny]==0:
                     visited[nx][ny]=visited[x][y]+1
                     queue.append((nx,ny))
 
