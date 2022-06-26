e94c3e3488a3cdcdbbdc0b82ad29efe6904f85a8
liean17 <94332594+liean17@users.noreply.github.com>
2022-06-26 13:37:40 +0900

    [7주차] <강도경> 두 수의 합

---
+++ 7주차/두 수의 합/dokyung.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class dokyung {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.valueOf(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.valueOf(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int x = Integer.valueOf(br.readLine());
+
+        //갯수
+        int count = 0;
+
+        //가장 작은 수
+        int start = 0;
+
+        //가장 큰 수
+        int end = n - 1;
+
+        //합
+        int sum = 0;
+
+        while(start < end) {
+            sum = arr[start] + arr[end];
+            if(sum == x)
+                count++;
+            if(sum <= x)
+                start++;
+            else
+                end--;
+        }
+
+        System.out.println(count);
+    }
+}
